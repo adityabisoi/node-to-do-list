888621dbf821fbb019ee6b85f56e3789e89a7bc8
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-09-05 13:42:46 +0530

    add item to databse

--- app.js
+++ app.js
@@ -1,34 +1,63 @@
-
 const express = require("express")
 const bodyParser = require("body-parser")
 const date = require(__dirname + "/date.js")
-
+const mongoose = require('mongoose')
 const app = express()
 
 app.set('view engine', 'ejs')
-
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(express.static("public"))
-
-const items = ["Buy Food", "Cook Food", "Eat Food"]
-
-app.get("/", function(req, res) {
-
-const day = date.getDate()
-
-  res.render("list", {listTitle: day, newListItems: items})
-
+mongoose.connect('mongodb://localhost:27017/todoDB', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 })
 
-app.post("/", function(req, res){
+const todoSchema = {
+  name: String
+}
 
+const Item = mongoose.model('Item', todoSchema)
+
+const item1 = new Item({
+  name: 'Practice Ds & Algo'
+})
+
+const item2 = new Item({
+  name: 'Study new stuff'
+})
+
+const item3 = new Item({
+  name: 'Develop software'
+})
+
+const defaultToDos = [item1, item2, item3]
+
+app.get("/", function (req, res) {
+  const day = date.getDate()
+  Item.find({}, (e, items) => {
+    if (items.length === 0) {
+      Item.insertMany(defaultToDos, (e) => {
+        console.log(e);
+      })
+    }
+    res.render("list", {
+      listTitle: day,
+      newListItems: items
+    })
+  })
+})
+
+app.post("/", function (req, res) {
   const item = req.body.newItem
-
-    items.push(item)
-    res.redirect("/")
-  
+  const newItem = new Item({
+    name: item
+  })
+  newItem.save()
+  res.redirect('/')
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000")
 })
